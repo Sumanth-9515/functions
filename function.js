944ac62f685e6d3198f68597e12d06e2cd3206d0
Sumanth-9515 <myakalasumanthreddy@gmail.com>
2024-10-22 19:16:35 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,69 @@
+
+function addition(a, b) {
+    var a = 10
+    var b = 20
+    return a + b;
+} console.log(addition());
+
+function subract(a, b) {
+    var a = 20
+    var b = 10
+    return a - b;
+} console.log(subract());
+
+
+function multiply(a, b) {
+    var a = 20
+    var b = 10
+    return a * b;
+} console.log(multiply());
+
+
+function divide(a, b) {
+    var a = 20
+    var b = 0
+    if (b == 0) {
+        return "cant divide by zero"
+    }
+    return a / b;
+} console.log(divide());
+
+
+// var a=140
+// var b=4
+// var name="sumanth"
+// function functionname(){
+//     var name="akash"
+//     var adress="hyd"
+// }
+// functionname()
+
+// var input = prompt("choose what you want")
+// switch (input) {
+//     case "home",
+//         console.log("Home is opened"):
+//         break;
+//     case "clasess",
+//         console.log("clasess is opened"):
+//         break;
+//     case "projects",
+//         console.log("projects is opened"):
+//         break;
+//     case "notes",
+//         console.log("notes is opened"):
+//         break;
+//     default:
+//         console.log("choose correct one");
+// }
+
+// var a=10;
+// function an(){
+//     ab=10
+//     ab=100
+//     ac=50
+//     console.log(ab);
+// }
+// console.log(ac);
+// console.log(a);
+// an();
+
